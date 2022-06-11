2c1ee7957b00979e8004449b99745e96148c1cde
delvinsaji <91379593+delvinsaji@users.noreply.github.com>
2022-06-11 16:18:06 +0300

    Updata ingredinet left

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -12,6 +12,11 @@ function Main() {
   const navigate = useNavigate();
   const [ingredients, setIngredients] = useState([]);
   const [ingredient, setIngredient] = useState();
+  const [prep, setPrep] = useState();
+  const [name, setName] = useState();
+  const [deleteing, setDeleteing] = useState([]);
+  const [adding, setAdding] = useState([]);
+
   useEffect(() => {
     axios
       .get("https://recipe1api.herokuapp.com/api/all_recipe")
@@ -21,7 +26,9 @@ function Main() {
       .catch((error) => {
         alert(error.data);
       });
+  }, []);
 
+  useEffect(() => {
     axios
       .get(`https://recipe1api.herokuapp.com/api/recipe/${recipe}`)
       .then((Response) => {
@@ -31,9 +38,8 @@ function Main() {
         alert(error.data);
       });
   }, [recipe]);
-  console.log(ingredients);
-  console.log(ingredient);
 
+  const [adup, setAdup] = useState(1);
   return (
     <div className="recipe">
       <div className="list">
@@ -55,6 +61,7 @@ function Main() {
         <p
           onClick={() => {
             setAdd("addrecipe1");
+            setAdup(1);
           }}
         >
           + Add Recipe
@@ -63,7 +70,31 @@ function Main() {
       <div className="desc">
         <div className="desc1">
           <h2>{recipe}</h2>
-          <p className="e">Update Recipe</p>
+          <div className="editbuttons">
+            <p
+              onClick={() => {
+                axios
+                  .get(`https://recipe1api.herokuapp.com/api/delete/${recipe}/`)
+                  .then((Response) => {
+                    alert(Response.data);
+                  });
+              }}
+              className="e"
+            >
+              Delete Recipe
+            </p>
+            <p
+              className="e"
+              onClick={() => {
+                setAdd("addrecipe1");
+                setAdup(2);
+                setName(recipe1.name);
+                setPrep(recipe1.prep);
+              }}
+            >
+              Update Recipe
+            </p>
+          </div>
         </div>
 
         <ul>
@@ -88,16 +119,41 @@ function Main() {
       <div className={add}>
         <div className="mainform">
           <p>Add Recipe</p>
-          <input type="text" placeholder="Name"></input>
-          {ingredients.map((obj) => (
-            <div className="ing">
-              <p>{obj}</p>
-              <img
-                src="https://cdn-icons.flaticon.com/png/128/1620/premium/1620739.png?token=exp=1654797391~hmac=477594fad0825da151a766ae15ed4bee"
-                width={10}
-              />
-            </div>
-          ))}
+          <input
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            type="text"
+            placeholder="Name"
+          ></input>
+          {ingredients
+            ? ingredients.map((obj) => (
+                <div className="ing">
+                  <p>{obj}</p>
+                  <img
+                    onClick={() => {
+                      if (adup === 1) {
+                        setIngredients(
+                          ingredients.filter(function (value) {
+                            return value !== obj;
+                          })
+                        );
+                      } else {
+                        setDeleteing([...deleteing, obj]);
+                        setIngredients(
+                          ingredients.filter(function (value) {
+                            return value !== obj;
+                          })
+                        );
+                      }
+                    }}
+                    src="https://cdn-icons.flaticon.com/png/128/1620/premium/1620739.png?token=exp=1654797391~hmac=477594fad0825da151a766ae15ed4bee"
+                    width={10}
+                  />
+                </div>
+              ))
+            : "null"}
           <input
             type="text"
             value={ingredient}
@@ -106,14 +162,55 @@ function Main() {
             }}
             placeholder="Ingredients"
             onKeyDown={(event) => {
-              if (event.key == "Enter") {
-                setIngredients([...ingredients, ingredient]);
-                setIngredient("");
+              if (event.key === "Enter") {
+                if (adup === 1) {
+                  setIngredients([...ingredients, ingredient]);
+                  setIngredient("");
+                } else {
+                  setAdding([...adding, ingredient]);
+                  setIngredient("");
+                }
               }
             }}
           ></input>
-          <textarea placeholder="Preparation" rows={10} cols={60}></textarea>
-          <button>Add Recipe</button>
+          <textarea
+            value={prep}
+            onChange={(e) => {
+              setPrep(e.target.value);
+            }}
+            placeholder="Preparation"
+            rows={10}
+            cols={60}
+          ></textarea>
+          <button
+            onClick={() => {
+              if (adup === 1) {
+                axios.post(
+                  "https://recipe1api.herokuapp.com/api/create_recipe",
+                  {
+                    name: name,
+                    prep: prep,
+                    ingredients: ingredients,
+                  }
+                );
+              } else {
+                console.log(deleteing);
+                console.log(adding);
+                axios.post(
+                  `https://recipe1api.herokuapp.com/api/update/${recipe}/`,
+                  {
+                    name: name,
+                    prep: prep,
+                    delete_ingredient: deleteing,
+                    create_ingredient: adding,
+                  }
+                );
+              }
+              setAdd("addrecipe");
+            }}
+          >
+            {adup === 1 ? "Add Recipe" : "Update Recipe"}
+          </button>
         </div>
       </div>
     </div>
